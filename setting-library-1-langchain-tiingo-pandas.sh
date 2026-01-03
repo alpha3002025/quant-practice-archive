65480792b72be0ad42230228bbf31d2e5ca90716
alpha3002025 <alpha300uk@gmail.com>
2026-01-04 08:39:32 +0900

    env: dependency

--- setting-library-1-langchain-tiingo-pandas.sh
+++ setting-library-1-langchain-tiingo-pandas.sh
@@ -1,1 +1,3 @@
-pip install langchain langchain-google-genai tiingo pandas
+## github workflow 내에서도 사용하기 위한 용도 
+#### (requirements.txt 가 환경별로 다르게 의존성이 구성될수도 있어서 별도의 pip 명령어로 구성)
+pip install langchain langchain-google-genai tiingo pandas keyring
